ee2ccafae5cb1b2765f1b1c7d5db51d83b6cfa87
yyliziqiu <yyliziqiu@163.com>
2024-02-21 14:25:24 +0800

    p xtime

--- xtime/time.go
+++ xtime/time.go
@@ -26,3 +26,39 @@ func GetTimestampOfYear(ts int64) int64 {
 	t := time.Unix(ts, 0)
 	return time.Date(t.Year(), 0, 0, 0, 0, 0, 0, time.Local).Unix()
 }
+
+func TodayRange() (time.Time, time.Time) {
+	year, month, day := time.Now().Date()
+	start := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+	stops := time.Now()
+	return start, stops
+}
+
+func TodayStartAt() time.Time {
+	start, _ := TodayRange()
+	return start
+}
+
+func YesterdayRange() (time.Time, time.Time) {
+	year, month, day := time.Now().AddDate(0, 0, -1).Date()
+	start := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+	stops := time.Date(year, month, day, 23, 59, 59, 0, time.Local)
+	return start, stops
+}
+
+func YesterdayStartAt() time.Time {
+	start, _ := YesterdayRange()
+	return start
+}
+
+func CurrentMonthRange() (time.Time, time.Time) {
+	year, month, _ := time.Now().Date()
+	start := time.Date(year, month, 1, 0, 0, 0, 0, time.Local)
+	stops := time.Now()
+	return start, stops
+}
+
+func CurrentMonthStartAt() time.Time {
+	start, _ := CurrentMonthRange()
+	return start
+}
